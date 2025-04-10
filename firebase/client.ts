19744f07429e2ff575b9bae44052c260d980d0c0
Leonid Gutman <l.gutman94@gmail.com>
2025-04-10 14:04:58 +0200

    completed section 1 (introduction & setup)

---
+++ firebase/client.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, FirebaseApp } from "firebase/app";
+import { Auth, getAuth } from "firebase/auth";
+import { FirebaseStorage, getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDMvNeFvnFKWA97PX_cvCOX_vKqewmy58E",
+    authDomain: "nextjs-15-and-firebase-6d96e.firebaseapp.com",
+    projectId: "nextjs-15-and-firebase-6d96e",
+    storageBucket: "nextjs-15-and-firebase-6d96e.firebasestorage.app",
+    messagingSenderId: "1045152423994",
+    appId: "1:1045152423994:web:05a82e40370769c31b5503"
+};
+
+// Initialize Firebase
+const currentApps  : FirebaseApp[] = getApps();
+
+const app =
+  currentApps && currentApps.length ?
+      currentApps[0] :
+      initializeApp(firebaseConfig);
+
+const auth: Auth = getAuth(app);
+const storage: FirebaseStorage = getStorage(app);
+
+export { auth, storage };
+
---
+++ firebase/server.ts
@@ -0,0 +1,30 @@
+import admin from "firebase-admin";
+import { App, getApps, ServiceAccount } from "firebase-admin/app";
+import { Firestore, getFirestore } from "firebase-admin/firestore";
+
+const serviceAccount = {
+    "type": "service_account",
+    "project_id": "nextjs-15-and-firebase-6d96e",
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40nextjs-15-and-firebase-6d96e.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+}
+
+// Initialize Firebase
+const currentApps  : App[] = getApps();
+const app =
+    currentApps && currentApps.length ?
+        currentApps[0] :
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount as ServiceAccount)
+        });
+
+const firestore: Firestore = getFirestore(app);
+
+export { firestore};
