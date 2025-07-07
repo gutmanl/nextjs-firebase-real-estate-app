be1dab409178d91f84187c9539378b8b540daa48
Leonid Gutman <l.gutman94@gmail.com>
2025-07-07 15:12:47 +0200

    intermediate commit - required to perform migration to TailwindCSS v4

---
+++ components/auth-buttons.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import {useAuth} from "@/context/auth";
+import Link from "next/link";
+
+export default function AuthButtons() {
+    const auth = useAuth();
+
+    return (
+        <div>
+            {!!auth?.currentUser &&
+              <>
+                <div>{auth.currentUser.email}</div>
+                <div onClick={() => {auth.logout();}}>Logout</div>
+              </>
+            }
+            {
+                !auth?.currentUser &&
+                <>
+                  <div className="flex gap-2 items-center">
+                    <Link href="/login" className="uppercase tracking-widest hover:underline">Log In</Link>
+                    <div className="h-8 w-[1px] bg-white/50"></div>
+                    <Link href="/register" className="uppercase tracking-widest hover:underline">Sign Up</Link>
+                  </div>
+                </>
+            }
+        </div>
+    );
+}
